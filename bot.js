4373d2726d32489cf620c4346bb4c2850af2e76f
aidanhayas <43485913+aidanhayas@users.noreply.github.com>
2018-09-21 19:33:09 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,34 @@
+{
+  "ClientId": 492442616970084353,
+  "Token": "NDkyNDQyNjE2OTcwMDg0MzUz.DoWkqQ.EhSpF1qc1xPpTJDw3OaBqPI7Fi4",
+  "OwnerIds": [
+    455767304861384706,
+    481197647751806996 
+  ],
+  "LoLApiKey": "",
+  "GoogleApiKey": "AIzaSyCYX3L0ShXQIyuUJxnXwGusyxLFe-HsMGw",
+  "MashapeKey": "",
+  "OsuApiKey": "",
+  "SoundCloudClientId": "",
+  "CleverbotApiKey": "",
+  "CarbonKey": "",
+  "Db": {
+    "Type": "sqlite",
+    "ConnectionString": "Data Source=data/NadekoBot.db"
+  },
+  "TotalShards": 1,
+  "PatreonAccessToken": "",
+  "PatreonCampaignId": "334038",
+  "RestartCommand": null,
+  "ShardRunCommand": "",
+  "ShardRunArguments": "",
+  "ShardRunPort": null,
+  "MiningProxyUrl": null,
+  "MiningProxyCreds": null,
+  "BotListToken": null,
+  "TwitchClientId": null,
+  "VotesToken": null,
+  "VotesUrl": null,
+  "RedisOptions": null
+}
+client.login(process.env.BOT_TOKEN);
